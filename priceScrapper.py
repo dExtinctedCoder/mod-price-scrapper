9bcaa140c39f0d7fe96c8a1324865e75af5a748e
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-07-23 17:55:18 +0100

    Error handling

--- priceScrapper.py
+++ priceScrapper.py
@@ -1,39 +1,47 @@
-from itertools import count, product
-import re
 import requests
 from bs4 import BeautifulSoup
 
+#iphone 7 link
+iphone7_link = 'https://www.jumia.com.ng/apple-renewed-iphone-7-plus-32gbram3gb-black-iphone7p-118774089.html'
+
+#iphone 8 link
+# iphone8_link='https://www.jumia.com.ng/apple-renewed-iphone-8-64gb3gb-ram-gold-iphone8-102114632.html'
+
+# #Just add the product link here and fetch the price
+# iphone13_link = 'https://www.jumia.com.ng/iphone-13-pro-6.1-inch-super-retina-xdr-display-with-promotion-6gb-ram-256gb-rom-ios-15-5g-facetime-sierra-blue-apple-mpg1620490.html'
+# samsung_link = 'https://www.jumia.com.ng/samsung-galaxy-a23-6.6-tft-screen-4gb64gb-memory-50mp-quad-camera-5000-mah-battery-android-12-blue-106402124.html'
+# techno_link = 'https://www.jumia.com.ng/tecno-pova2-battery-7000mah-color-dazzle-black-132972301.html'
+# itel17_link = 'https://www.jumia.com.ng/itel-17-pro-3g-galaxy-blue-128769853.html'
+# hot11_link = 'https://www.jumia.com.ng/infinix-hot-11s-6.7-ips-lcd-screen-4gb64gb-memory-50mp-triple-camera-5000-mah-battery-android-11-green-wave-114778541.html'
+# note12_link = 'https://www.jumia.com.ng/infinix-note-12i-4gb128gb-memory-force-black-127336449.html'
+wrong_link = 'https://abcdef'
+
+def fetch_jumia_product(product_link):
+    jumia_product_url= product_link
+
+    try:
+        page = requests.get(url=jumia_product_url)
+    except:
+        return 'Page Not Found'
+    # page = requests.get(url=jumia_product_url)
+    soup=BeautifulSoup(page.content,'lxml')
+    price =soup.find('span', class_='-b -ltr -tal -fs24').text
+    phone={
+        'price':price
+    }
+    return phone["price"]
 
 
-product = {}
 
-def scrapper(keyword):
-  site_url=f"https://www.jumia.com.ng/catalog/?q={keyword}"
-  page = requests.get(url=site_url)
-  soup=BeautifulSoup(page.content,'lxml')
 
-  for a in soup.findAll('a', href=True, attrs={'class': 'core'}):
-    price = a.find('div', attrs={'class': 'prc'})
-    name = a.find('h3', attrs={'class': 'name'})
-    strip_price = str(price.text)
-    strip_name = str(name.text)
-    x = strip_price[2:]
-    transformed_string = re.sub(",","",x)
-    price_num = int(transformed_string)
-    product[strip_name] = price_num
-
-  for key in product:
-    print(key, '->', product[key])
-    print('')
-
-  
-
-def extract(jeyword):
-    scrapedword=str(scrapper(jeyword))
-
-    print(scrapedword)
-
-extract('iphone' + '11')
-extract('samsung')
+print(fetch_jumia_product(iphone7_link))
+print(fetch_jumia_product(wrong_link))
+# print(fetch_jumia_product(iphone8_link))
+# print(fetch_jumia_product(iphone13_link))
+# print(fetch_jumia_product(samsung_link))
+# print(fetch_jumia_product(techno_link))
+# print(fetch_jumia_product(itel17_link))
+# print(fetch_jumia_product(hot11_link))
+# print(fetch_jumia_product(note12_link))
 
 
